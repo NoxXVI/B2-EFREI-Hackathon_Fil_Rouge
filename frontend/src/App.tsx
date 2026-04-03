1f3f23aa3054df23138526830a8bc4dcdd876a52
Timothé Leroy <77456834+NoxXVI@users.noreply.github.com>
2026-04-03 11:37:23 +0200

    New assets and tamagui config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
+import { TamaguiProvider, YStack } from "tamagui";
+import config from "../tamagui.config";
 import { GameCanvas } from "./game/GameCanvas";
 
 export default function App() {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#000",
-      }}
-    >
-      <GameCanvas />
-    </div>
+    <TamaguiProvider config={config} defaultTheme="light">
+      <YStack
+        width="100vw"
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="$background"
+      >
+        <GameCanvas />
+      </YStack>
+    </TamaguiProvider>
   );
 }
